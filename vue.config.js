44ba4b278ebf4dfc38f83b4cb741ef07e00a2099
Antoine Meeus <antoine.meeus@mytheresa.com>
2020-09-11 23:54:58 +0200

    fix vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 // vue.config.js
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/SearchBeerApp-VueCLI/'
-    : '/'
+  devServer: {
+    port: 8080 // CHANGE YOUR PORT HERE!
+  }
 }
